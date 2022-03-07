0f8c276e56ed1141cd76b3c9690c62658e387650
Paul Zhang <zhangpaul1023@hotmail.com>
2022-03-07 00:32:23 -0800

    'changetimestamp'

--- transaction_server/transactions/models.py
+++ transaction_server/transactions/models.py
@@ -32,7 +32,7 @@ class Transaction(models.Model):
 		DISPLAY_SUMMARY = 'DISPLAY_SUMMARY'
 
 	datatime = datetime.datetime.now()
-	timestamp = datatime.timestamp()
+	timestamp = int(datatime.timestamp())
 	server = models.CharField(max_length=64)
 	command = models.CharField(choices=Command.choices, max_length=16)
 	quoteServerTime = models.DecimalField(decimal_places=2, max_digits=24, null=True)
