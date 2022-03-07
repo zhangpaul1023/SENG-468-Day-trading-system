a223a6efeb976547bc219e59856ef1f1c5168cbf
Paul Zhang <zhangpaul1023@hotmail.com>
2022-03-06 20:55:58 -0800

    'fix_data_type_error'

--- transaction_server/transactions/views.py
+++ transaction_server/transactions/views.py
@@ -34,7 +34,7 @@ def QuoteServer(userid, stock_symbol):
 	returndata = []
 	HOST = '192.168.4.2'
 	PORT = 4444
-	dataSend = userid + stock_symbol + "\n"
+	dataSend = str(userid) + " " + str(stock_symbol) + "\n"
 	dataSend = str.encode(dataSend)
 	with socket.socket(socket.AF_INET, socket.SOCK_STREAM)as s:
 		s.connect((HOST, PORT))
