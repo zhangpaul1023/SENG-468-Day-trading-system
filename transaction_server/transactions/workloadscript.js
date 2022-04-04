691c4db3e7c3b49060118b09beb0ff2eb922f595
Cole Dodd <cole@ruboss.com>
2022-04-03 23:26:12 -0700

    workload

---
+++ transaction_server/transactions/workloadscript.js
@@ -0,0 +1,117 @@
+import http from 'k6/http';
+
+export default function () {
+  function randomSymbol() {
+    function randomLetter() {
+      const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      return alphabet[Math.floor(Math.random() * alphabet.length)];
+    }
+    return `${randomLetter()}${randomLetter()}${randomLetter()}`;
+  }
+
+  const user = `user${__VU}`;
+  const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const symbol = `${alphabet[Math.floor(Math.random() * alphabet.length)]}${alphabet[Math.floor(Math.random() * alphabet.length)]}${alphabet[Math.floor(Math.random() * alphabet.length)]}`;
+  while (true) {
+    http.get(`http://localhost:8000/transactions/add/${user}/63511.53/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/quote/${user}/${symbol}/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/buy/${user}/${symbol}/276.83/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/commit_buy/${user}/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/commit_sell/${user}/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/cancel_sell/${user}/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/quote/${user}/${symbol}/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/add/${user}/45016.23/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/commit_buy/${user}/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/commit_buy/${user}/`); sleep(1);
+    //http.get(`http://localhost:8000/transactions/display_summary/${user}/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/add/${user}/71879.98/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/buy/${user}/${symbol}/303.83/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/commit_buy/${user}/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/commit_buy/${user}/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/commit_buy/${user}/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/commit_sell/${user}/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/add/${user}/74315.15/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/cancel_buy/${user}/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/cancel_set_buy/${user}/${symbol}/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/commit_sell/${user}/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/buy/${user}/${symbol}/51.49/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/commit_buy/${user}/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/commit_sell/${user}/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/buy/${user}/${symbol}/156.07/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/commit_buy/${user}/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/buy/${user}/${symbol}/575.27/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/cancel_sell/${user}/`); sleep(1);
+    //http.get(`http://localhost:8000/transactions/display_summary/${user}/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/set_buy_amount/${user}/${symbol}/658.38/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/sell/${user}/${symbol}/641.90/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/commit_sell/${user}/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/commit_buy/${user}/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/cancel_set_sell/${user}/${symbol}/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/commit_sell/${user}/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/cancel_buy/${user}/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/cancel_sell/${user}/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/buy/${user}/${symbol}/264.17/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/quote/${user}/${symbol}/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/buy/${user}/${symbol}/657.49/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/commit_buy/${user}/`); sleep(1);
+    //http.get(`http://localhost:8000/transactions/display_summary/${user}/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/buy/${user}/${symbol}/710.47/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/commit_buy/${user}/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/buy/${user}/${symbol}/208.11/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/commit_buy/${user}/`); sleep(1);
+    //http.get(`http://localhost:8000/transactions/display_summary/${user}/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/add/${user}/52802.93/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/buy/${user}/${symbol}/433.22/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/set_sell_trigger/${user}/${symbol}/59.23/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/buy/${user}/${symbol}/387.59/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/commit_buy/${user}/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/cancel_buy/${user}/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/commit_buy/${user}/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/commit_sell/${user}/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/cancel_set_buy/${user}/${symbol}/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/cancel_set_buy/${user}/${symbol}/`); sleep(1);
+    //http.get(`http://localhost:8000/transactions/display_summary/${user}/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/quote/${user}/${symbol}/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/quote/${user}/${symbol}/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/sell/${user}/${symbol}/429.74/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/set_sell_trigger/${user}/${symbol}/51.92/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/buy/${user}/${symbol}/384.67/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/commit_buy/${user}/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/commit_buy/${user}/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/buy/${user}/${symbol}/357.57/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/cancel_buy/${user}/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/set_sell_amount/${user}/${symbol}/216.83/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/cancel_set_sell/${user}/${symbol}/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/buy/${user}/${symbol}/516.29/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/cancel_buy/${user}/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/sell/${user}/${symbol}/41.47/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/set_sell_trigger/${user}/${symbol}/30.04/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/commit_sell/${user}/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/cancel_set_buy/${user}/${symbol}/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/cancel_set_sell/${user}/${symbol}/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/buy/${user}/${symbol}/289.68/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/cancel_set_buy/${user}/${symbol}/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/add/${user}/87863.73/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/quote/${user}/${symbol}/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/buy/${user}/${symbol}/333.91/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/commit_buy/${user}/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/sell/${user}/${symbol}/213.24/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/buy/${user}/${symbol}/427.36/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/cancel_sell/${user}/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/buy/${user}/${symbol}/730.08/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/commit_buy/${user}/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/buy/${user}/${symbol}/36.79/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/commit_buy/${user}/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/sell/${user}/${symbol}/644.65/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/cancel_sell/${user}/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/commit_buy/${user}/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/cancel_set_sell/${user}/${symbol}/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/cancel_sell/${user}/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/cancel_set_sell/${user}/${symbol}/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/buy/${user}/${symbol}/749.86/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/commit_buy/${user}/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/cancel_buy/${user}/`); sleep(1);
+    http.get(`http://localhost:8000/transactions/sell/${user}/${symbol}/664.10/`); sleep(1);
+    //http.get(`http://localhost:8000/transactions/dumplog/./testlog`); sleep(1);
+  }
+}
