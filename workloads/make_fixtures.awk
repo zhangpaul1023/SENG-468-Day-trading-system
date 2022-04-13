e42a07f8a64d58d6757c396dc90066a7d21fbec5
Cole Dodd <cole@ruboss.com>
2022-04-12 20:54:28 -0700

    removed workload

--- workloads/make_fixtures.awk
+++
@@ -1,8 +0,0 @@
-{
-	print "- model: transactions.User"
-	print "  pk: " NR
-	print "  fields:"
-	print "    userid: " $1
-	print "    funds: 0"
-}
-
--- workloads/make_fixtures.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-awk '{split($2,arr,","); print(arr[2]);}' final_workload_2019 | uniq > /tmp/usernames.txt
-awk -f make_fixtures.awk /tmp/usernames.txt > users.yaml
-rm /tmp/usernames.txt
